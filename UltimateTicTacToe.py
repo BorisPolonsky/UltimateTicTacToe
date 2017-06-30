7a5de7f947f3122b4fcd0043c6f6a0f226fe3073
Boris Polonsky <qdogan@163.com>
2017-06-30 17:06:48 +0800

    Minor Update
    Added rule set.
    Other bug fix and corresonding changes

--- UltimateTicTacToe.py
+++ UltimateTicTacToe.py
@@ -2,42 +2,56 @@ import copy
 class UltimateTicTacToe():
     class TicTacToe():
         def __init__(self, **kwargs):
-            self.slots=[[" "]*3 for i in range(3)]
-            self.occupancy=None#"O" , "X" or "draw"
-            self.numFilledSlots=0
+            self.__slots=[[" "]*3 for i in range(3)]
+            self.__occupancy=None#"O" , "X" or "draw"
+            self.__numFilledSlots=0
             return super().__init__(**kwargs)
 
         def Take(self,row,column,side):
             """
-            rtype: bool. Return True if a side wins the TicTacToe. Return False if draw or not full.  
+            rtype: bool. Return True if:
+            A side wins the TicTacToe or draw. 
             """
             if not (row in [0,1,2] and column in [0,1,2]):
                 raise ValueError("Invalid coordinate.")
-            if (self.slots[row][column]==" "):
+            if (self.__slots[row][column]==" "):
                 if side=="O" or side=="X":
-                    self.numFilledSlots+=1
-                    self.slots[row][column]=side
+                    self.__numFilledSlots+=1
+                    self.__slots[row][column]=side
                     #Searh row, column and diagonal
-                    if (self.slots[row][0]==self.slots[row][1] and self.slots[row][1]==self.slots[row][2])\
-                    or (self.slots[0][column]==self.slots[1][column] and self.slots[1][column]==self.slots[2][column])\
-                    or (row==column and self.slots[0][0]==self.slots[1][1] and self.slots[1][1]==self.slots[2][2])\
-                    or (row+column==2 and self.slots[0][2]==self.slots[1][1] and self.slots[1][1]==self.slots[2][0]):
-                        self.occupancy=side
+                    if (self.__slots[row][0]==self.__slots[row][1] and self.__slots[row][1]==self.__slots[row][2])\
+                    or (self.__slots[0][column]==self.__slots[1][column] and self.__slots[1][column]==self.__slots[2][column])\
+                    or (row==column and self.__slots[0][0]==self.__slots[1][1] and self.__slots[1][1]==self.__slots[2][2])\
+                    or (row+column==2 and self.__slots[0][2]==self.__slots[1][1] and self.__slots[1][1]==self.__slots[2][0]):
+                        self.__occupancy=side
                         return True
-                    if self.numFilledSlots==9:
-                        self.occupancy="draw"
+                    if self.__numFilledSlots==9:
+                        self.__occupancy="draw"
+                        return True
                     return False
                 else:
                     raise ValueError("Invalid input for side.")
             else:
                 raise ValueError("The slot is already occupied!")
 
+        def slot(self):
+            return [row[:] for row in self.__slots]
+
+        def occupancy(self):
+            return self.__occupancy
+
     def __init__(self, **kwargs):
-        self.blocks=[[UltimateTicTacToe.TicTacToe() for i in range(3)]for i in range(3)]
-        self.occupancy=[[None for i in range(3)] for i in range(3)]
-        self.nextSide=None
-        self.nextBlock=None
-        self.numFilledBlocks=0# Total number of blocks that can not be filled. 
+        self.__blocks=[[UltimateTicTacToe.TicTacToe() for i in range(3)]for i in range(3)]
+        self.__occupancy=[[None for i in range(3)] for i in range(3)]
+        self.__nextSide=None
+        self.__nextBlock=None
+        self.__numFilledBlocks=0# Total number of blocks that can not be filled. 
+        if ("sovereignityUponDraw" in kwargs)==False:
+            self.__sovereignityUponDraw="none"
+        elif kwargs["sovereignityUponDraw"] in ("none","both"):
+            self.__sovereignityUponDraw=kwargs["sovereignityUponDraw"]
+        else:
+            raise ValueError('Invalid rule set. "sovereignityUponDraw" should be either "both" or "none".')
         return super().__init__(**kwargs)
 
     def Take(self,**kwargs):
@@ -50,43 +64,53 @@ class UltimateTicTacToe():
         kwargs["columnSlot"]-=1
         if not (kwargs["rowBlock"] in [0,1,2] and kwargs["columnBlock"] in [0,1,2]):
             raise ValueError("Invalid coordinate.")
-        if (self.nextBlock!=None) and (self.blocks[kwargs["rowBlock"]][kwargs["columnBlock"]] is self.nextBlock)==False:
+        if (self.__nextBlock!=None) and (self.__blocks[kwargs["rowBlock"]][kwargs["columnBlock"]] is self.__nextBlock)==False:
             raise ValueError("Rule violation. Invalid block")
-        if self.blocks[kwargs["rowBlock"]][kwargs["columnBlock"]].occupancy==None:
-            if (kwargs["side"]=="O" or kwargs["side"]=="X") and (self.nextSide==None or self.nextSide==kwargs["side"]):
-                winStat=self.blocks[kwargs["rowBlock"]][kwargs["columnBlock"]].Take(kwargs["rowSlot"],kwargs["columnSlot"],kwargs["side"])
-                self.nextSide="O" if kwargs["side"]=="X" else "X"
-                self.nextBlock=None if self.blocks[kwargs["rowSlot"]][kwargs["columnSlot"]].occupancy!=None else self.blocks[kwargs["rowSlot"]][kwargs["columnSlot"]]
-                if winStat==True:
+        if self.__blocks[kwargs["rowBlock"]][kwargs["columnBlock"]].occupancy()==None:
+            if (kwargs["side"]=="O" or kwargs["side"]=="X") and (self.__nextSide==None or self.__nextSide==kwargs["side"]):
+                terminal=self.__blocks[kwargs["rowBlock"]][kwargs["columnBlock"]].Take(kwargs["rowSlot"],kwargs["columnSlot"],kwargs["side"])
+                self.__nextSide="O" if kwargs["side"]=="X" else "X"
+                self.__nextBlock=None if self.__blocks[kwargs["rowSlot"]][kwargs["columnSlot"]].occupancy()!=None else self.__blocks[kwargs["rowSlot"]][kwargs["columnSlot"]]
+                if terminal==True:
+                    self.__numFilledBlocks+=1
+                    horizontal=(self.__blocks[kwargs["rowBlock"]][0].occupancy(),self.__blocks[kwargs["rowBlock"]][1].occupancy(),self.__blocks[kwargs["rowBlock"]][2].occupancy())
+                    vertical=(self.__blocks[0][kwargs["columnBlock"]].occupancy(),self.__blocks[1][kwargs["columnBlock"]].occupancy(),self.__blocks[2][kwargs["columnBlock"]].occupancy())
+                    diagonal1=(self.__blocks[0][0].occupancy(),self.__blocks[1][1].occupancy(),self.__blocks[2][2].occupancy())
+                    diagonal2=(self.__blocks[2][0].occupancy(),self.__blocks[1][1].occupancy(),self.__blocks[0][2].occupancy())
+                    if self.__sovereignityUponDraw=="both":
+                        horizontal=map(lambda x:x if x!="draw" else kwargs["side"],horizontal)
+                        vertical=map(lambda x:x if x!="draw" else kwargs["side"],vertical)
+                        diagonal1=map(lambda x:x if x!="draw" else kwargs["side"],diagonal1)
+                        diagonal2=map(lambda x:x if x!="draw" else kwargs["side"],diagonal2)
                     #Search row, column and diagonal
-                    self.numFilledBlocks+=1
-                    if (self.blocks[kwargs["rowBlock"]][0].occupancy==self.blocks[kwargs["rowBlock"]][1].occupancy and self.blocks[kwargs["rowBlock"]][1].occupancy==self.blocks[kwargs["rowBlock"]][2].occupancy)\
-                    or (self.blocks[0][kwargs["columnBlock"]].occupancy==self.blocks[1][kwargs["columnBlock"]].occupancy and self.blocks[1][kwargs["columnBlock"]].occupancy==self.blocks[2][kwargs["columnBlock"]].occupancy)\
-                    or (kwargs["rowBlock"]==kwargs["columnBlock"] and self.blocks[0][0].occupancy==self.blocks[1][1].occupancy and self.blocks[1][1].occupancy==self.blocks[2][2].occupancy)\
-                    or (kwargs["rowBlock"]+kwargs["columnBlock"]==2 and self.blocks[0][2].occupancy==self.blocks[1][1].occupancy and self.blocks[1][1].occupancy==self.blocks[2][0].occupancy):
-                        self.occupancy=kwargs["side"]        
-                        self.nextSide=None
-                        self.nextBlock=None
+                    if (horizontal[0]==horizontal[1] and horizontal[1]==horizontal[2])\
+                    or (vertical[0]==vertical[1] and vertical[1]==vertical[2])\
+                    or (kwargs["rowBlock"]==kwargs["columnBlock"] and diagonal1[0]==diagonal1[1] and diagonal1[1]==diagonal1[2])\
+                    or (kwargs["rowBlock"]+kwargs["columnBlock"]==2 and diagonal2[0]==diagonal2[1] and diagonal2[1]==diagonal2[2]):
+                        self.__occupancy=kwargs["side"]        
+                        self.__nextSide=None
+                        self.__nextBlock=None
                         return True
-                    return False
-                elif self.blocks[kwargs["rowBlock"]][kwargs["columnBlock"]].occupancy!=None:#draw
-                    self.numFilledBlocks+=1
-                    if self.numFilledBlocks==9:
-                        self.occupancy="draw"
-                    return False
+                    elif self.__numFilledBlocks==9:
+                        self.__occupancy=="draw"
+                        self.__nextSide=None
+                        self.__nextBlock=None
             else:
                 raise ValueError("Invalid input for side.")
         else:
             raise ValueError("Invalid block selection. Block occupied.")
 
+    def occupancy(self):
+        return self.__occupancy
+
     def __repr__(self):
         wholeUltimateTicTacToe=[]      
-        for rowBlock in self.blocks:
+        for rowBlock in self.__blocks:
             rowUltimateTicTacToeBlocks=[]
             for rowSlot in range(3):
                 rowUltimateTicTacToe=[]
                 for columnBlock in range(3):
-                    rowUltimateTicTacToe.append(" | ".join(rowBlock[columnBlock].slots[rowSlot]))
+                    rowUltimateTicTacToe.append(" | ".join(rowBlock[columnBlock].slot()[rowSlot]))
                 rowUltimateTicTacToeBlocks.append(" "+"||".join(rowUltimateTicTacToe)+" ")
             wholeUltimateTicTacToe.append("\n---------------------------------\n".join(rowUltimateTicTacToeBlocks))
         return "\n=================================\n".join(wholeUltimateTicTacToe)+"\n"
@@ -107,4 +131,8 @@ if __name__=="__main__":
     T.Take(rowBlock=2,columnBlock=2,rowSlot=1,columnSlot=1,side="X")
     print(T)
     T.Take(rowBlock=1,columnBlock=1,rowSlot=3,columnSlot=3,side="O")
+    print(T)
+    T.Take(rowBlock=3,columnBlock=3,rowSlot=1,columnSlot=1,side="X")
+    print(T)
+    T.Take(rowBlock=2,columnBlock=2,rowSlot=3,columnSlot=3,side="O")
     print(T)

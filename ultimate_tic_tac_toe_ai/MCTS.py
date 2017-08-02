10ab90c822fba301156d7845946dbfdb5fe8fc70
BorisPolonsky <qdogan@163.com>
2017-08-02 18:15:46 +0800

    Minor Update
    Implemented the training procedures for MCTS.

--- ultimate_tic_tac_toe_ai/MCTS.py
+++ ultimate_tic_tac_toe_ai/MCTS.py
@@ -2,42 +2,151 @@ import pickle
 from UltimateTicTacToe import *
 import time
 import os
+import math
+from enum import Enum,unique
 class MCT:
     class NodeMCT:
+        @unique
+        class Result(Enum):
+            initiatorWins=0
+            initiatorLoses=1
+            draw=2
         def __init__(self, op= None):
             """
             Initialization of a node.
             self.__op: the operation from last state to reach this node.
             None for root, (rowBlock, rowColumn, rowSlot, columnSlot) for the rest.
-            self.__val: a tuple of (#game in which the initiator wins+0.5*#draw, #total games)
+            self.__val: a tuple of (#game in which the initiator wins, #game in which the initiator loses, #total games)
+            #draw=self.__val[2]-self.__val[0]-self.__val[1]
             self.__next: a list of children of the node.
             """
-            if op is None or (type(op)==tuple and len(op)==4):
+            if (type(op)==tuple and len(op)==4):
+                for val in op:
+                    if val not in range(1, 4):
+                        raise ValueError("Each entry of the state must be an integer from 1-3")
+                self.__op= op
+            elif op is None:
                 self.__op= op
             else:
                 raise TypeError('Parameter "op" must be either None or a tuple of (rowBlock, rowColumn, rowSlot, columnSlot)')
-            self.__val=(0,0)
+            self.__val=(0,0,0)
             self.__next=[]
 
-        def addChild(self,action):
-            node=MCT.NodeMCT(action)
+        def addChild(self, action):
+            node = MCT.NodeMCT(action)
             self.__next.append(node)
+            return node
+
+        def update(self,result):
+            if result==MCT.NodeMCT.Result.initiatorWins:
+                self.__val= (self.__val[0]+1, self.__val[1], self.__val[2]+1)
+            elif result==MCT.NodeMCT.Result.initiatorLoses:
+                self.__val= (self.__val[0]+1, self.__val[1]+1, self.__val[2]+1)
+            elif result==MCT.NodeMCT.Result.draw:
+                self.__val= (self.__val[0], self.__val[1], self.__val[2]+1)  # ?
+            else:
+                raise ValueError("Invalid result. ")
 
         @property
         def children(self):
             return self.__next[:]
 
+        @property
+        def state(self):
+            return self.__op
+
+        @property
+        def record(self):
+            return self.__val
+
+    @unique
+    class Stage(Enum):
+        selection=0
+        simulation=1
+        backPropogation=2
+
     def __init__(self):
         """
         Create an empty MCT.
+        self.__root: root of the tree
+        self.__size: total number of nodes (except root node) in the tree
         """
         self.__root=MCT.NodeMCT()
+        self.__size = 0
 
-    def trainMCT(tree,epochNum=20):
+    @classmethod
+    def trainMCT(cls, tree, epochNum=20):
         """
         epoch: Total number of game rounds simluated.
         """
-        pass
+        nExploration, nExploitation=0, 0
+        for epoch in range(1,epochNum+1):
+            print("Training epoch: {}".format(epoch))
+            terminal=False
+            currentNode=tree.__root
+            initiator, opponent="X", "O"
+
+            currentSide=initiator
+            board=UltimateTicTacToe()
+            stage=MCT.Stage.selection
+            path=[currentNode]
+            while not terminal:
+                print(board)
+                # Selection
+                if stage == MCT.Stage.selection:
+                    exploredActions=[node.state for node in currentNode.children]
+                    validActions=board.validActions
+                    actionsToBeExplored = list(set(validActions) - set(exploredActions))
+                    if len(list(set(exploredActions)-set(validActions)))>0:
+                        raise RuntimeError("Explored invalid actions!")
+                    if len(actionsToBeExplored)>0:
+                        action = random.choice(actionsToBeExplored)
+                        stage=MCT.Stage.simulation
+                        currentNode=tree.__addNode(currentNode, action)
+                        path.append(currentNode)
+                        nExploration+=1
+                    else:
+                        if currentSide == initiator:
+                            scores = [node.record[0] / node.record[2] + math.sqrt(
+                                2 * math.log(currentNode.record[2]) / node.record[2]) for node in currentNode.children]
+                        else:
+                            scores = [node.record[1] / node.record[2] + math.sqrt(
+                                2 * math.log(currentNode.record[2]) / node.record[2]) for node in currentNode.children]
+                        scoreNodePairs=zip(scores, currentNode.children)
+                        # Select action with highest score
+                        currentNode=max(scoreNodePairs, key=lambda x:x[0])[1]
+                        path.append(currentNode)
+                        action=currentNode.state
+                        nExploitation+=1
+                    print("Taking action {}".format(action))
+                    terminal=board.take(*action, currentSide)
+
+                elif stage == MCT.Stage.simulation:
+                    action= board.randomAction
+                    currentNode=tree.__addNode(currentNode, action)
+                    print("Taking action {}".format(action))
+                    terminal=board.take(*action, currentSide)
+                    path.append(currentNode)
+                    nExploration+=1
+
+                currentSide="X" if currentSide=="O" else "O"
+            # back-propagation
+            if board.occupancy==initiator:
+                result=MCT.NodeMCT.Result.initiatorWins
+            elif board.occupancy==opponent:
+                result=MCT.NodeMCT.Result.initiatorLoses
+            elif board.occupancy=="draw":
+                result=MCT.NodeMCT.Result.draw
+            else:
+                raise ValueError("Failed to get expected result.")
+            for node in path:
+                node.update(result)
+        return nExploitation, nExploration
+
+    def __addNode(self,parent,action):
+        node=parent.addChild(action)
+        self.__size+=1
+        return node
 
     @classmethod
     def saveModel(cls, tree, modelPath):
@@ -48,15 +157,25 @@ class MCT:
                 pickle.dump(tree,fileObj)
         except IOError as e:
             print(e)
+    @property
+    def size(self):
+        return self.__size
 
-def loadModel(**kwargs):
-    if "modelPath" in kwargs:
+    @classmethod
+    def loadModel(cls,modelPath):
         try:
-            with open(os.path(kwargs["modelPath"]),"rb") as fileObj:
+            with open(os.path.normpath(modelPath),"rb") as fileObj:
                 return pickle.load(fileObj)
         except IOError as e:
             print(e)
-    return None
+            return None
 
-if __name__=="__main__":
+if __name__ == "__main__":
+    path=r".\model\test.pkl"
+    tree = MCT.loadModel(path)
+    result=MCT.trainMCT(tree, 10000)
+    print("Size of knowledge base: {}\n".format(tree.size))
+    print("#Explitation:{}\n#Exploration:{}\n".format(result[0],result[1]))
+    MCT.saveModel(tree, path)
     pass
+

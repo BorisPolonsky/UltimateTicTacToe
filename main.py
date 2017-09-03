d5e7705e2d589f972c2a65b2c92a73cd41ee9e4f
BorisPolonsky <qdogan@163.com>
2017-09-04 03:42:47 +0800

    Minor Update
    Added configuration file for the program.
    Renamed the models.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from ultimate_tic_tac_toe import game_manager
 from ultimate_tic_tac_toe.mcts import MCT
+import configparser
+import os
 if __name__ == "__main__":
+    conf = configparser.ConfigParser()
+    conf.read("config")
     print("Welcome!")
     while True:
         try:
-            rule=int(input("Select rule set! \n1: Standard \n2: Bizarre \n"))
+            rule = int(input("Select rule set! \n1: Standard \n2: Bizarre \n"))
         except Exception: #  might be fixed
             print("Invalid input for rule set. ")
         else:
@@ -12,12 +16,14 @@ if __name__ == "__main__":
                 break
             else:
                 continue
-    modelPath= r"./model/test-rule{}.pkl".format(rule)
+    modelNameMapping = {1: conf.get("model_config", "model_name_normal_rule"),
+                        2: conf.get("model_config", "model_name_bizarre_rule")}
+    modelPath = os.path.normpath(os.path.join(conf.get("model_config", "model_dir"), modelNameMapping[rule]))
     print("Loading model...")
-    game=game_manager.GameManager(modelPath)
+    game = game_manager.GameManager(modelPath)
     print("Done!")
     while True:
-        result=input(r"Do you want to play as initiator?[Y/N]")
+        result = input(r"Do you want to play as initiator?[Y/N]")
         if result == "Y" or result == "y":
             asInitiator = True
             break
@@ -27,12 +33,12 @@ if __name__ == "__main__":
         else:
             print('The expected input is enter either Y or N, got "{}"'.format(result))
     while True:
-        result=input(r"Which side do you want to play as?[O/X]")
-        if result=="O" or result=="o":
-            side="O"
+        result = input(r"Which side do you want to play as?[O/X]")
+        if result == "O" or result=="o":
+            side ="O"
             break
-        elif result=="X" or result=="x":
-            side="X"
+        elif result == "X" or result=="x":
+            side = "X"
             break
         else:
             print('The expected input is enter either O or X, got "{}"'.format(result))
